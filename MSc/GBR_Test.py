6464f67f01ed17d716a28c126d8d252756fe0202
florus82 <poetzscf@hu-berlin.de>
2018-10-08 17:55:25 +0200

    Check predictor performance

--- MSc/GBR_Test.py
+++ MSc/GBR_Test.py
@@ -1,6 +1,7 @@
 from FloppyToolZ.Funci import *
 import pandas as pd
 from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.externals import joblib
 from sklearn import metrics
@@ -17,5 +18,7 @@ fil   = [getFilelist(path, '.csv') for path in paths]
 filp  = [f for fi in fil for f in fi]
 
 test = pd.read_csv(filp[0])
-x = test.drop(['Mean_AGB' ,'mean_AGB'], axis=1)
-y = test['Mean_AGB']
+x_dat = test.drop(['Mean_AGB' ,'mean_AGB'], axis=1)
+y_dat = test['Mean_AGB']
+
+x_train, x_test, y_train, y_test = train_test_split(x_dat, y_dat, random_state= 42, test_size = 0.3)
