ed3c620cc06005c9d01a074c43bb71503666ddd4
Floppy <poetzscf@hu-berlin.de>
2018-11-03 11:27:02 +0100

    go hard

--- MSc/MODIS_cubes_sav_prediction.py
+++ MSc/MODIS_cubes_sav_prediction.py
@@ -1,40 +1,35 @@
 from FloppyToolZ.MasterFuncs import *
 
 tile  = 'Mod1'
-path  = 'Y:/_students_data_exchange/FP_FP/Seafile/myLibrary/MSc/'
-cubis = getFilelist(path + 'Modelling/prediction/megadump/' + tile + '/SP', '.sav')
-tili  = gdal.Open('Y:/_students_data_exchange/FP_FP/RS_Data/MODIS/rasterized/Mod1.tiff')
+path  = '/home/florus/Seafile/myLibrary/MSc/'
+cubis = getFilelist(path + 'Modelling/Single_VIs/prediction/' + tile + '/SP', '.sav')
+tili  = gdal.Open('/home/florus/MSc/RS_Data/MODIS/rasterized/Mod1.tiff')
 til   = tili.GetRasterBand(1).ReadAsArray()
 
 # load models for lowest,median and highest RMSE) - should all be part of apply_along_axis
-iter100 = pd.read_csv(path + 'Modelling/All_VIs/runs100/AllRuns.csv')
-savs    = getFilelist(path + 'Modelling/All_VIs/runs100/sav', '.sav')
+iter100 = pd.read_csv(path + 'Modelling/Single_VIs/Modelling/runs100/AllRuns.csv')
+savs    = getFilelist(path + 'Modelling/Single_VIs/Modelling/runs100/sav', '.sav')
 sav     = getMinMaxMedianSAV(iter100, savs)
 
-
-test = cubis[0]
+test = cubis[122]
 tes = joblib.load(test)
 
 pred_arr = np.empty((tes.shape[0] * tes.shape[1],tes.shape[2]))
 for dim3 in range(tes.shape[2]):
    pred_arr[:,dim3] = tes[:,:,dim3].ravel()
 
-aa = np.ma.masked_where(np.isnan(pred_arr) == True, pred_arr)
-predi = sav[1][1][0][0].predict()
+res_arr = np.where(np.isnan(pred_arr) == True, np.nan, 1)
+pred_arr = pred_arr[np.logical_not(np.isnan(pred_arr))]
+pred_arr = pred_arr.reshape(int(pred_arr.shape[0]/res_arr.shape[1]), res_arr.shape[1])
 
 
-
-a=np.array([7,1,10,0,5,4,5,0,9,9,3,3])
-b=np.array([10,9,3,0,4,7,7,0,8,1,10,9])
-a=a.reshape(12,1)
-b=b.reshape(12,1)
-maa = np.ma.masked_array(a, mask=[0, 0, 0, 1,0, 0,0,1,0,0,0,0])
-mbb = np.ma.masked_array(b, mask=[0, 0, 0, 1,0, 0,0,1,0,0,0,0])
-maa= np.ma.compressed(maa).reshape(10,1)
-mbb= np.ma.compressed(mbb).reshape(10,1)
+predi = sav[1][2].predict(pred_arr)
 
 
-
+r, c = np.where(np.isnan(pred_arr) == True)
+r[0:20]
+c[0:20]
+print(str(r[30*7]) + '_' + str(c[0]))
 
 
 stack_array = np.ones((rows * cols, bandsALL), dtype=np.int16)
