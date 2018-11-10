5247794219058a89a19a4faa09e12be25d8c1462
florus82 <poetzscf@hu-berlin.de>
2018-11-10 17:10:32 +0100

    latest

--- MSc/Raster_Resamp.py
+++ MSc/Raster_Resamp.py
@@ -1,22 +1,24 @@
 from FloppyToolZ.MasterFuncs import *
 
-aa = gdal.Open('/home/florus/Seafile/myLibrary/MSc/Modelling/Single_VIs/prediction/Mod1/Mod1_EoS_Max.tif')
+mos_path = 'Z:/_students_data_exchange/FP_FP/Seafile/myLibrary/MSc/other_biomass _maps/TC_SC'
+mos = 'SC_Landsat-Sentinel.tif'
+aa = gdal.Open(mos_path + '/' + mos)
 bb = aa.GetRasterBand(1).ReadAsArray()
-
-aggFac = 2
+bb[bb == -9999] = np.nan
+aggFac = 3
 
 # check if raster has even dimenensions for cutting out 2x2 squaree
-if bb.shape[0] % 2 != 0:
+while bb.shape[0] / aggFac % 2 != 0:
     dd = np.empty((1, bb.shape[1]))
     dd.fill(np.nan)
     bb = np.concatenate([bb, dd])
-if bb.shape[1] % 2 != 0:
+while bb.shape[1] / aggFac % 2 != 0:
     dd = np.empty((bb.shape[0], 1))
     dd.fill(np.nan)
     bb = np.concatenate([bb, dd],axis=1)
 
 # create empty array for aggregation output
-arr = np.empty((int(bb.shape[0]/2), int(bb.shape[1]/2)))
+arr = np.empty((int(bb.shape[0]/aggFac), int(bb.shape[1]/aggFac)))
 
 for row in range(0, bb.shape[0], 2):
     for col in range(0,bb.shape[1], 2):
@@ -24,7 +26,9 @@ for row in range(0, bb.shape[0], 2):
 
 # write raster
 gtiff_driver = gdal.GetDriverByName('GTiff')
-out_ds = gtiff_driver.Create('/home/florus/MSc/trash/resamp.tif', arr.shape[1], arr.shape[0], 1, aa.GetRasterBand(1).DataType)
+out_ds = gtiff_driver.Create(('/').join(mos_path.split('/')[0:len(mos_path.split('/'))-1]) + '/resample/' +
+                mos.split('.')[0] + 'resample_fac' + str(aggFac) +'.tif',
+                             arr.shape[1], arr.shape[0], 1, aa.GetRasterBand(1).DataType)
 out_gt = list(aa.GetGeoTransform())
 out_gt[1], out_gt[5] = out_gt[1] * aggFac, out_gt[5]*aggFac
 out_ds.SetGeoTransform(out_gt)
