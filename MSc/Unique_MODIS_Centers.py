c7f9cc83c9d650dc353ce089ad5d94c2034100f8
poetzscf@hu-berlin.de <lAShjk34!lkjf%?sdfSLA>
2019-05-29 17:19:26 +0200

    2019-05-29

--- MSc/Unique_MODIS_Centers.py
+++ MSc/Unique_MODIS_Centers.py
@@ -36,4 +36,35 @@ for i in range(1,len(pod['X'])):
 
 
 df = pd.DataFrame(data=pod)
-df.to_csv('Y:/_students_data_exchange/FP_FP/Seafile/myLibrary/MSc/2nd_round/plot_coords/test3.csv', sep=',',index=False)
+df.to_csv('Y:/_students_data_exchange/FP_FP/Seafile/myLibrary/MSc/2nd_round/plot_coords/test4.csv', sep=',',index=False)
+
+# create an ordered shapefile
+
+driv = ogr.GetDriverByName('ESRI Shapefile')  # will select the driver foir our shp-file creation.
+shapeStor = driv.CreateDataSource('/'.join(points.split('/')[:-1]))
+# get first layer (assuming ESRI is standard) & and create empty output layer with
+out_lyr = shapeStor.CreateLayer(points.split('/')[-1].split('.')[0] + '_ordered_XY',
+                                getSpatRefVec(points), poin.GetGeomType())
+
+# create attribute field
+out_lyr.CreateFields(poin.schema)
+dups_fld = ogr.FieldDefn('Dups', ogr.OFTString)
+dups_fld.SetWidth(8)
+out_lyr.CreateField(dups_fld)
+
+# with attributes characteristics
+out_feat = ogr.Feature(out_lyr.GetLayerDefn())
+
+for in_feat in poin:
+
+    out_feat.SetGeometry(in_feat.geometry())
+    # copy existing attributes
+    for i in range(in_feat.GetFieldCount()):
+        out_feat.SetField(i, in_feat.GetField(i))
+    # input for new attribute
+    out_feat.SetField(in_feat.GetFieldCount(), pod['dups'][pod.get('UniqueID').index(in_feat.GetField('UniqueID'))])
+    #create feature
+    out_lyr.CreateFeature(out_feat)
+poin.ResetReading()
+shapeStor.Destroy()
+del poin
